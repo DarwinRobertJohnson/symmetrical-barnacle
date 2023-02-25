6b5a7be8edea90218c0cbe0ce4c42738936a5adf
Darwin Robert Johnson <darwinrj852003@gmail.com>
2023-02-25 23:49:07 +0530

    add insert functionality

---
+++ app.java
@@ -0,0 +1,38 @@
+import java.lang.*;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+
+
+public class app{
+
+    public static void addEntry() throws Exception{
+        Class.forName("org.sqlite.JDBC");
+        Connection con=DriverManager.getConnection("jdbc:sqlite:databaseFiles/revision");
+        Statement stm=con.createStatement();
+        Scanner sc=new Scanner(System.in);
+
+        String topic=new String();
+        topic=sc.nextLine();
+        
+        PreparedStatement state=con.prepareStatement("insert into subject1 values(?,?,?)");
+        System.out.println("statement prepared");
+        state.setString(1,topic);
+        System.out.println("num1 set");
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        state.setString(2, date);
+        System.out.println("num2 set");
+        state.setInt(3,0);
+        System.out.println("num3 set");
+
+        state.executeUpdate();
+        state.close();
+        stm.close();
+        con.close();
+    }
+
+    public static void main(String[] args) throws Exception{
+        addEntry();
+    }
+}
