6cfa2bc03777c44b5f137e79359688e907ad2c52
Darwin Robert Johnson <darwinrj852003@gmail.com>
2023-02-28 19:59:10 +0530

    Fix the next revision date function and incorporate it with the add entry function

--- app.java
+++ app.java
@@ -14,15 +14,14 @@ public class app{
 
         int monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
 
-        int month=Integer.parseInt(str.substring(5,7));
-        int day=Integer.parseInt(str.substring(8,10));
+        int month=Integer.parseInt(str.substring(3,5));
+        int day=Integer.parseInt(str.substring(0,2));
         int mult=1;
         for(int i=0;i<num_revision;i++){
             mult*=2;
         }
 
         day=day+mult;
-
         if(day>monthDays[month-1]){
             day=day-monthDays[month-1];
             month++;
@@ -38,7 +37,7 @@ public class app{
             dayString="0"+day;
         }
 
-        next_rev_date=str.substring(0,5)+monthString+"-"+dayString;
+        next_rev_date=dayString+"-"+monthString+str.substring(5,10);
 
         return next_rev_date;
     }
@@ -52,14 +51,19 @@ public class app{
         String topic=new String();
         topic=sc.nextLine();
         
+        String firstRevisionDate;
+
         PreparedStatement state=con.prepareStatement("insert into subject1 values(?,?,?,0)");
         System.out.println("statement prepared");
         state.setString(1,topic);
         System.out.println("num1 set");
         String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        System.out.println(date);    //Local Date
         state.setString(2, date);
         System.out.println("num2 set");
-        state.setInt(3,0);
+        firstRevisionDate=revisionDate(date, 0);
+        System.out.println("First revision date got");
+        state.setString(3,firstRevisionDate);
         System.out.println("num3 set");
 
         state.executeUpdate();
@@ -69,7 +73,7 @@ public class app{
     }
 
     public static void main(String[] args) throws Exception{
-        //addEntry();
-        System.out.println(revisionDate("2023-02-20",0));
+        addEntry();
+        //System.out.println(revisionDate("28-02-2023",0));
     }
 }
